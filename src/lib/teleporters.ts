1af16d925a5277cf94edf49a0767166150bac2b3
RickyBenevelli <rickybenevelli@gmail.com>
2025-02-03 17:28:09 +0100

    fix: removed tabs stuff

--- src/lib/teleporters.ts
+++
@@ -1,8 +0,0 @@
-// teleporters.ts
-import { createTeleporter } from 'react-teleporter';
-
-export const Tab1Teleporter = createTeleporter();
-export const Tab2Teleporter = createTeleporter();
-export const Tab3Teleporter = createTeleporter();
-export const GraphTeleporter = createTeleporter();
-export const ControlPanelTeleporter = createTeleporter();
